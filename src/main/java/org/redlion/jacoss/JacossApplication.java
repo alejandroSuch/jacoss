ef93c38b62ff76c4be1afb9ff95eaaa5ab18ecf1
Alejandro Such <alejandro.such@gmail.com>
2016-04-15 16:39:39 +0200

    Auto-generated project data

---
+++ src/main/java/org/redlion/jacoss/JacossApplication.java
@@ -0,0 +1,12 @@
+package org.redlion.jacoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacossApplication.class, args);
+	}
+}
