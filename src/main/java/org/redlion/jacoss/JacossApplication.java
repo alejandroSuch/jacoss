526c54f909e7b7c4f1ce51889df82d0fa5e490fb
Alejandro Such <alejandro.such@gmail.com>
2016-04-17 21:34:38 +0200

    Added @ComponentScan to main Application

--- src/main/java/org/redlion/jacoss/JacossApplication.java
+++ src/main/java/org/redlion/jacoss/JacossApplication.java
@@ -2,7 +2,9 @@ package org.redlion.jacoss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 @SpringBootApplication
 public class JacossApplication {
 
